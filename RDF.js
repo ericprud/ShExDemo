326d66326e8006dee1c188aaf234e0f0bf033142
ericprud <eric@w3.org>
2015-02-26 17:52:49 -0500

    ~ can't construct Triple with null terms

--- RDF.js
+++ RDF.js
@@ -3450,7 +3450,7 @@ RDF = {
 
                             // record the success.
                             RDF.message(s.toString() + " is a " + ruleLabel.toString());
-                            var t = RDF.Triple(s, RDF.IRI("http://open-services.net/ns/core#instanceShape", RDF.Position0()), ruleLabel);
+                            var t = {s:s, p:RDF.IRI("http://open-services.net/ns/core#instanceShape", RDF.Position0()), o:ruleLabel};
                             ret.matchedTree(this.ruleMap[ruleLabel], t, res);
                         }
                     }
